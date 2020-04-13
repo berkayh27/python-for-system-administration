e5647b4b46718f90e98581fac971ed4903dd7856
EC2 Default User <ec2-user@ip-172-31-8-97.eu-west-2.compute.internal>
2020-04-13 06:00:01 +0000

    added array

--- HackerRank/array.py
+++ HackerRank/array.py
@@ -2,14 +2,15 @@ import os
 import sys 
 
 # This is used to sum items in array
+# any array that is given will print out the to
 
 array1 = [1,2,3,4,5]
 
-def arraySummer(ar):
-    zeroo = 0
-    for elements in ar:
-        zeroo += elements
-    return zeroo
+def arraySummer(anything_that_user_gives):
+    total_now = 0
+    for elements in anything_that_user_gives:
+        total_now += elements
+    return total_now
 
 output_for_me = arraySummer(array1)
 print(output_for_me)
