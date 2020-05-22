70207b98427f3d2d34c255b6ce87164f8644fe02
farrukh90 <farrukhsadykov@gmail.com>
2020-05-22 05:02:53 +0000

    added task15

---
+++ practicepython.org/task15.py
@@ -0,0 +1,15 @@
+# reverse() function takes the results and prints out in reverse order. 
+
+test_string = ["This", "test"]
+test_string.reverse()
+print(test_string)
+
+
+
+# Below code takes a string and creates  a list out of it
+new_string = "This is a list of strings that I am playing with"
+new_new_string = new_string.split()
+print(new_new_string)
+
+for i in new_new_string:
+    print(i)
